6f9a0991818c1417f9ded433ce1b8200f3e22f52
JFMaturana <jfmaturana@gmail.com>
2017-10-23 21:58:37 -0400

    Initial commit

---
+++ app/src/main/java/ca/jfmcode/pass_note/LaunchActivity.java
@@ -0,0 +1,13 @@
+package ca.jfmcode.pass_note;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+    }
+}
